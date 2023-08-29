058495575f3dc889a24378b3b318e777aec01bb2
hothuuk <s22014@gsm.hs.kr>
2023-08-29 17:57:19 +0900

    :tada: 시작

---
+++ src/main/java/com/ezel/voza/VozaApplication.java
@@ -0,0 +1,13 @@
+package com.ezel.voza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VozaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VozaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ezel/voza/VozaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezel.voza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VozaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
