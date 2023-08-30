df999b475d629b2e4285a1f4b6c3d6b8594d9efb
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-08-30 19:59:02 +0900

    #11 ⚡️ user coloum 명 변경

--- src/main/java/com/ezel/voza/domain/user/entity/User.java
+++ src/main/java/com/ezel/voza/domain/user/entity/User.java
@@ -14,7 +14,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "userId")
+    @Column(name = "user_id")
     private Long id;
 
     @Column(nullable = true)
