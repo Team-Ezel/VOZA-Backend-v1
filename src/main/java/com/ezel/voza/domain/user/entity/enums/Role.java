0d9d6d8d6462315afb25ddb746bcf324ab55320a
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-08-30 20:32:36 +0900

    📝 AuthDetails 작성

--- src/main/java/com/ezel/voza/domain/user/entity/enums/Role.java
+++ src/main/java/com/ezel/voza/domain/user/entity/enums/Role.java
@@ -1,10 +1,16 @@
 package com.ezel.voza.domain.user.entity.enums;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import org.springframework.security.core.GrantedAuthority;
 
-public enum Role {
+public enum Role implements GrantedAuthority {
     ROLE_USER, ROLE_ADMIN;
 
     @JsonCreator
     public static Role from(String s) { return Role.valueOf(s.toUpperCase()); }
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
 }
---
+++ src/main/java/com/ezel/voza/global/security/auth/AuthDetails.java
@@ -0,0 +1,50 @@
+package com.ezel.voza.global.security.auth;
+
+import com.ezel.voza.domain.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@RequiredArgsConstructor
+public class AuthDetails implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(user.getRole());
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
