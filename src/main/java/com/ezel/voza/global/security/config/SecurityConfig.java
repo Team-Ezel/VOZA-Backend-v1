48ba07147088eee3263781c9faf62e039c5c6d37
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-08-30 21:00:48 +0900

    📝 SecurityConfig에 JwtFilter 추가

--- src/main/java/com/ezel/voza/global/security/config/SecurityConfig.java
+++ src/main/java/com/ezel/voza/global/security/config/SecurityConfig.java
@@ -1,15 +1,21 @@
 package com.ezel.voza.global.security.config;
 
+import com.ezel.voza.global.filter.JwtRequestFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final JwtRequestFilter jwtRequestFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception {
@@ -22,6 +28,9 @@ public class SecurityConfig {
                 .authorizeHttpRequests()
                 .requestMatchers("/oauth2/**").permitAll()
                 .anyRequest().denyAll();
+
+        http
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
