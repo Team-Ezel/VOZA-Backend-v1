14c81c7fee82ce3ba5733ef4fc968e10bdd56db7
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-08-30 09:59:40 +0900

    📝 InfoService에 KakaToken redis에 올리는 로직 추가

--- src/main/java/com/ezel/voza/domain/kakao/service/GetKakaoUserInfoService.java
+++ src/main/java/com/ezel/voza/domain/kakao/service/GetKakaoUserInfoService.java
@@ -1,7 +1,9 @@
 package com.ezel.voza.domain.kakao.service;
 
+import com.ezel.voza.domain.kakao.entity.KakaoToken;
 import com.ezel.voza.domain.kakao.presentation.dto.response.KakaoTokenResponse;
 import com.ezel.voza.domain.kakao.presentation.dto.response.KakaoUserInfoResponse;
+import com.ezel.voza.domain.kakao.repository.KakaoTokenRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -14,6 +16,7 @@ public class GetKakaoUserInfoService {
     private final WebClient webClient;
     private static final String USER_INFO_URI = "https://kapi.kakao.com/v2/user/me";
     private final GetKakaoTokenService getKakaoTokenService;
+    private final KakaoTokenRepository kakaoTokenRepository;
 
     public KakaoUserInfoResponse getUserInfo(String code) {
 
@@ -24,6 +27,9 @@ public class GetKakaoUserInfoService {
                 .header("Authorization", "Bearer " + kakaoTokenResponse.getAccess_token())
                 .retrieve()
                 .bodyToFlux(KakaoUserInfoResponse.class);
+
+        KakaoToken kakaoToken = new KakaoToken(response.blockFirst().getId(), kakaoTokenResponse.getAccess_token());
+        kakaoTokenRepository.save(kakaoToken);
 
         return response.blockFirst();
     }
