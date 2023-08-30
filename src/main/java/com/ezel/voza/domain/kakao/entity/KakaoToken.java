7671b2c47df6ccf60bcce1fb3f5815bcff910ac2
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-08-30 17:38:00 +0900

    ⚡️ KakaoToken @Id 추가

--- src/main/java/com/ezel/voza/domain/kakao/entity/KakaoToken.java
+++ src/main/java/com/ezel/voza/domain/kakao/entity/KakaoToken.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
 @Getter
@@ -13,6 +14,7 @@ import org.springframework.data.redis.core.RedisHash;
 @RedisHash("KakaoToken")
 public class KakaoToken {
 
+    @Id
     private Long userId;
 
     private String accessToken;
