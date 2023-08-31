bc552bfd23c15cd6956853f9fe3bad2f6c854628
hothuuk <s22014@gsm.hs.kr>
2023-08-31 18:23:31 +0900

    :memo: UpdateBoardRequest 작성

---
+++ src/main/java/com/ezel/voza/domain/board/presentation/dto/request/UpdateBoardRequest.java
@@ -0,0 +1,19 @@
+package com.ezel.voza.domain.board.presentation.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UpdateBoardRequest {
+
+    @NotBlank(message = "제목은 필수입니다.")
+    private String title;
+
+    @NotBlank(message = "내용은 필수입니다.")
+    private String content;
+}
