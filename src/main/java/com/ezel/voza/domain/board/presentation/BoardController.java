e2b96d721530029612fbe53b66d95ea622642ea3
hothuuk <s22014@gsm.hs.kr>
2023-08-31 20:24:43 +0900

    :memo: findDetailOne 추가

--- src/main/java/com/ezel/voza/domain/board/presentation/BoardController.java
+++ src/main/java/com/ezel/voza/domain/board/presentation/BoardController.java
@@ -2,8 +2,10 @@ package com.ezel.voza.domain.board.presentation;
 
 import com.ezel.voza.domain.board.presentation.dto.request.CreateBoardRequest;
 import com.ezel.voza.domain.board.presentation.dto.request.UpdateBoardRequest;
+import com.ezel.voza.domain.board.presentation.dto.response.DetailBoardResponse;
 import com.ezel.voza.domain.board.presentation.dto.response.ListBoardResponse;
 import com.ezel.voza.domain.board.service.CreateBoardService;
+import com.ezel.voza.domain.board.service.GetBoardDetailService;
 import com.ezel.voza.domain.board.service.ListBoardService;
 import com.ezel.voza.domain.board.service.UpdateBoardService;
 import jakarta.validation.Valid;
@@ -22,6 +24,8 @@ public class BoardController {
 
     private final UpdateBoardService updateBoardService;
 
+    private final GetBoardDetailService getBoardDetailService;
+
     @PostMapping
     public ResponseEntity<Void> create(@RequestBody @Valid CreateBoardRequest createBoardRequest) {
         createBoardService.execute(createBoardRequest);
@@ -32,6 +36,12 @@ public class BoardController {
     public ResponseEntity<ListBoardResponse> findAll() {
         var list = listBoardService.execute();
         return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DetailBoardResponse> findDetailOne(@PathVariable Long id) {
+        DetailBoardResponse oneFindById = getBoardDetailService.execute(id);
+        return new ResponseEntity<>(oneFindById, HttpStatus.OK);
     }
 
     @PatchMapping("/{id}")
