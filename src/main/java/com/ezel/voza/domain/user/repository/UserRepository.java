38fcabcd02bb8c7fcf09834c4813fd3f44215d79
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-09-05 20:35:52 +0900

    #23 UserRepository 에 existbyemail추가

--- src/main/java/com/ezel/voza/domain/user/repository/UserRepository.java
+++ src/main/java/com/ezel/voza/domain/user/repository/UserRepository.java
@@ -9,5 +9,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByNickName(String nickName);
 
+    Boolean existByEmail(String email);
+
     Optional<User> findByEmail(String email);
 }
