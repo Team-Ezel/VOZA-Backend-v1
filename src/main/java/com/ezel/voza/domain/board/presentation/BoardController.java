d2852b377b8da6b86dd52cc0cf64472b890a19a9
hothuuk <s22014@gsm.hs.kr>
2023-09-05 20:16:00 +0900

    :memo: delete 추가

--- src/main/java/com/ezel/voza/domain/board/presentation/BoardController.java
+++ src/main/java/com/ezel/voza/domain/board/presentation/BoardController.java
@@ -4,10 +4,7 @@ import com.ezel.voza.domain.board.presentation.dto.request.CreateBoardRequest;
 import com.ezel.voza.domain.board.presentation.dto.request.UpdateBoardRequest;
 import com.ezel.voza.domain.board.presentation.dto.response.DetailBoardResponse;
 import com.ezel.voza.domain.board.presentation.dto.response.ListBoardResponse;
-import com.ezel.voza.domain.board.service.CreateBoardService;
-import com.ezel.voza.domain.board.service.GetBoardDetailService;
-import com.ezel.voza.domain.board.service.ListBoardService;
-import com.ezel.voza.domain.board.service.UpdateBoardService;
+import com.ezel.voza.domain.board.service.*;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -25,6 +22,8 @@ public class BoardController {
     private final UpdateBoardService updateBoardService;
 
     private final GetBoardDetailService getBoardDetailService;
+
+    private final DeleteBoardService deleteBoardService;
 
     @PostMapping
     public ResponseEntity<Void> create(@RequestBody @Valid CreateBoardRequest createBoardRequest) {
@@ -48,5 +47,11 @@ public class BoardController {
     public ResponseEntity<Void> update(@PathVariable Long id, @RequestBody @Valid UpdateBoardRequest updateBoardRequest) {
         updateBoardService.execute(id, updateBoardRequest);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        deleteBoardService.execute(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
