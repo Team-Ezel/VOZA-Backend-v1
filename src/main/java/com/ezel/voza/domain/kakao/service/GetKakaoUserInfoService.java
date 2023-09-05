2f99a1525b62418b19ca01e5b70b7a0513506ba0
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-09-05 20:36:28 +0900

    #23 GetKakaoUserInfoService에 block 처리 추가

--- src/main/java/com/ezel/voza/domain/kakao/service/GetKakaoUserInfoService.java
+++ src/main/java/com/ezel/voza/domain/kakao/service/GetKakaoUserInfoService.java
@@ -4,7 +4,9 @@ import com.ezel.voza.domain.kakao.entity.KakaoToken;
 import com.ezel.voza.domain.kakao.presentation.dto.response.KakaoTokenResponse;
 import com.ezel.voza.domain.kakao.presentation.dto.response.KakaoUserInfoResponse;
 import com.ezel.voza.domain.kakao.repository.KakaoTokenRepository;
+import com.ezel.voza.domain.user.entity.User;
 import com.ezel.voza.domain.user.presentation.dto.request.SignUpRequest;
+import com.ezel.voza.domain.user.repository.UserRepository;
 import com.ezel.voza.domain.user.service.UserSignupService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -26,6 +28,7 @@ public class GetKakaoUserInfoService {
     private final GetKakaoTokenService getKakaoTokenService;
     private final UserSignupService userSignupService;
     private final KakaoTokenRepository kakaoTokenRepository;
+    private final UserRepository userRepository;
 
     public KakaoUserInfoResponse getUserInfo(String code) {
 
@@ -40,14 +43,16 @@ public class GetKakaoUserInfoService {
         KakaoToken kakaoToken = new KakaoToken(response.blockFirst().getId(), kakaoTokenResponse.getAccess_token(), kakaoTokenResponse.getRefresh_token(), kakaoTokenResponse.getExpires_in().longValue());
         kakaoTokenRepository.save(kakaoToken);
 
-        SignUpRequest signUpRequest = SignUpRequest.builder()
-                .email(Objects.requireNonNull(response.blockFirst()).getKakao_account().getEmail())
-                .profileUrl(Objects.requireNonNull(response.blockFirst()).getKakao_account().getProfile().getProfile_image_url())
-                .nickName(Objects.requireNonNull(response.blockFirst()).getKakao_account().getProfile().getNickname())
-                .build();
+        if (!userRepository.existByEmail(Objects.requireNonNull(response.blockFirst()).getKakao_account().getEmail())) {
+            SignUpRequest signUpRequest = SignUpRequest.builder()
+                    .email(Objects.requireNonNull(response.blockFirst()).getKakao_account().getEmail())
+                    .profileUrl(Objects.requireNonNull(response.blockFirst()).getKakao_account().getProfile().getProfile_image_url())
+                    .nickName(Objects.requireNonNull(response.blockFirst()).getKakao_account().getProfile().getNickname())
+                    .build();
 
-        userSignupService.execute(signUpRequest);
-
+            userSignupService.execute(signUpRequest);
+        }
+        
         return response.blockFirst();
     }
 }
