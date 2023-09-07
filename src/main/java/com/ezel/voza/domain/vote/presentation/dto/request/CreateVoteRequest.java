a2a46252261a2a9ae4f309dcc11f58771cb6d598
hothuuk <s22014@gsm.hs.kr>
2023-09-08 08:01:04 +0900

    :memo: NotBlank NotEmpty로 변경

--- src/main/java/com/ezel/voza/domain/vote/presentation/dto/request/CreateVoteRequest.java
+++ src/main/java/com/ezel/voza/domain/vote/presentation/dto/request/CreateVoteRequest.java
@@ -2,6 +2,7 @@ package com.ezel.voza.domain.vote.presentation.dto.request;
 
 import com.ezel.voza.domain.vote.entity.VoteOption;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,6 +18,6 @@ public class CreateVoteRequest {
     @NotBlank(message = "제목은 필수 요소입니다.")
     private String title;
 
-    @NotBlank(message = "투표항목은 최소 한개이상 입력해야합니다.")
+    @NotEmpty(message = "투표항목은 최소 한개이상 입력해야합니다.")
     private List<VoteOption> options;
 }
