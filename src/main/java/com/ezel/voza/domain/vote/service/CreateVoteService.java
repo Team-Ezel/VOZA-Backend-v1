5022c45b3125c7537948b2d281f793e92d17605d
hothuuk <s22014@gsm.hs.kr>
2023-09-08 11:03:05 +0900

    :memo: id 매게변수 추가

--- src/main/java/com/ezel/voza/domain/vote/service/CreateVoteService.java
+++ src/main/java/com/ezel/voza/domain/vote/service/CreateVoteService.java
@@ -2,7 +2,9 @@ package com.ezel.voza.domain.vote.service;
 
 import com.ezel.voza.domain.vote.presentation.dto.request.CreateVoteRequest;
 
+import java.util.UUID;
+
 public interface CreateVoteService {
 
-    void execute(CreateVoteRequest createVoteRequest);
+    void execute(CreateVoteRequest createVoteRequest, UUID id);
 }
