672f90ae73835f4cc1c51b61f5779d407426dec6
hothuuk <s22014@gsm.hs.kr>
2023-09-08 11:01:22 +0900

    :memo: RequestMapping 수정

--- src/main/java/com/ezel/voza/domain/vote/presentation/dto/VoteController.java
+++ src/main/java/com/ezel/voza/domain/vote/presentation/dto/VoteController.java
@@ -6,19 +6,22 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.UUID;
+
 @RequiredArgsConstructor
-@RequestMapping("/vote")
+@RequestMapping("group/{group_id}/vote")
 public class VoteController {
 
     private final CreateVoteService createVoteService;
 
     @PostMapping
-    public ResponseEntity<Void> create(@RequestBody @Valid CreateVoteRequest createVoteRequest) {
-        createVoteService.execute(createVoteRequest);
+    public ResponseEntity<Void> create(@RequestBody @Valid CreateVoteRequest createVoteRequest, @PathVariable UUID group_id) {
+        createVoteService.execute(createVoteRequest, group_id);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
