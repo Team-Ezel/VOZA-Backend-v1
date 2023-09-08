fe8dedcfc98c519cd0f7049cb38d85219c8d47f2
hothuuk <s22014@gsm.hs.kr>
2023-09-08 23:09:17 +0900

    :memo: findAll 추가

--- src/main/java/com/ezel/voza/domain/vote/presentation/dto/VoteController.java
+++ src/main/java/com/ezel/voza/domain/vote/presentation/dto/VoteController.java
@@ -1,15 +1,14 @@
 package com.ezel.voza.domain.vote.presentation.dto;
 
 import com.ezel.voza.domain.vote.presentation.dto.request.CreateVoteRequest;
+import com.ezel.voza.domain.vote.presentation.dto.response.ListVoteResponse;
 import com.ezel.voza.domain.vote.service.CreateVoteService;
+import com.ezel.voza.domain.vote.service.ListVoteService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -19,10 +18,18 @@ public class VoteController {
 
     private final CreateVoteService createVoteService;
 
+    private final ListVoteService listVoteService;
+
     @PostMapping
     public ResponseEntity<Void> create(@RequestBody @Valid CreateVoteRequest createVoteRequest, @PathVariable UUID group_id) {
         createVoteService.execute(createVoteRequest, group_id);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    private ResponseEntity<ListVoteResponse> findAll(@PathVariable UUID group_id) {
+        var list = listVoteService.execute(group_id);
+        return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
 }
