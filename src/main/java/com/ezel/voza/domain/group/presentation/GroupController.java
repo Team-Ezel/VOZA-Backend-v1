0c22e1264200bc2db338bbb92ebdbf000e30fb70
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-09-09 14:37:04 +0900

    #38 GroupController에 Enter 매핑 작성

--- src/main/java/com/ezel/voza/domain/group/presentation/GroupController.java
+++ src/main/java/com/ezel/voza/domain/group/presentation/GroupController.java
@@ -1,8 +1,10 @@
 package com.ezel.voza.domain.group.presentation;
 
 import com.ezel.voza.domain.group.presentation.dto.request.CreateGroupRequest;
+import com.ezel.voza.domain.group.presentation.dto.request.EnterGroupRequest;
 import com.ezel.voza.domain.group.service.CreateGroupInviteCode;
 import com.ezel.voza.domain.group.service.CreateGroupService;
+import com.ezel.voza.domain.group.service.EnterGroupService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,8 +19,8 @@ import java.util.UUID;
 public class GroupController {
 
     private final CreateGroupService createGroupService;
-
     private final CreateGroupInviteCode createGroupInviteCode;
+    private final EnterGroupService enterGroupService;
 
     @PostMapping
     public ResponseEntity<Void> groupCreate(@RequestBody @Valid CreateGroupRequest createGroupRequest) {
@@ -27,8 +29,14 @@ public class GroupController {
     }
 
     @PostMapping("/{groupId}")
-    public ResponseEntity<String> createInviteCode(@RequestParam UUID groupId) {
+    public ResponseEntity<?> createInviteCode(@PathVariable Long groupId) {
         String inviteCode = createGroupInviteCode.generateInviteCode(groupId);
         return new ResponseEntity<>(inviteCode, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/enter")
+    public ResponseEntity<Void> enterGroup(@RequestBody @Valid EnterGroupRequest enterGroupRequest) {
+        enterGroupService.execute(enterGroupRequest);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
