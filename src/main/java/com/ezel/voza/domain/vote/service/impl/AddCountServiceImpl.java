cf3dd621460d11627b3377fa0efcfcf17233701b
hothuuk <s22014@gsm.hs.kr>
2023-09-11 21:09:32 +0900

    :memo: 중복 투표 제거 추가

--- src/main/java/com/ezel/voza/domain/vote/service/impl/AddCountServiceImpl.java
+++ src/main/java/com/ezel/voza/domain/vote/service/impl/AddCountServiceImpl.java
@@ -1,11 +1,14 @@
 package com.ezel.voza.domain.vote.service.impl;
 
+import com.ezel.voza.domain.user.entity.User;
 import com.ezel.voza.domain.vote.entity.Vote;
 import com.ezel.voza.domain.vote.entity.VoteOption;
+import com.ezel.voza.domain.vote.exception.AlreadyVotedUserException;
 import com.ezel.voza.domain.vote.exception.VoteOptionMismatchException;
 import com.ezel.voza.domain.vote.presentation.dto.request.AddCountRequest;
 import com.ezel.voza.domain.vote.repository.VoteOptionRepository;
 import com.ezel.voza.domain.vote.service.AddCountService;
+import com.ezel.voza.global.util.UserUtil;
 import com.ezel.voza.global.util.VoteUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,6 +23,8 @@ public class AddCountServiceImpl implements AddCountService {
 
     private final VoteOptionRepository voteOptionRepository;
 
+    private final UserUtil userUtil;
+
     @Override
     public void execute(Long vote_id, AddCountRequest addCountRequest) {
 
@@ -27,8 +32,14 @@ public class AddCountServiceImpl implements AddCountService {
 
         VoteOption voteOption = voteOptionRepository.findOneById(addCountRequest.getId());
 
+        User user = userUtil.currentUser();
+
         if(!(Objects.equals(voteOption.getVote(), vote))) {
             throw new VoteOptionMismatchException();
+        }
+
+        if(!(voteOptionRepository.findUserById(user.getId()))) {
+            throw new AlreadyVotedUserException();
         }
 
         voteOption.addCount();
