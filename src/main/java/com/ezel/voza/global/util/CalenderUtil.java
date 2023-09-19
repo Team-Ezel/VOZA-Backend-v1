91e95606e3999152916cd7ad5e77ff41e8660b61
hothuuk <s22014@gsm.hs.kr>
2023-09-19 14:46:52 +0900

    :memo: CalenderUtil 작성

---
+++ src/main/java/com/ezel/voza/global/util/CalenderUtil.java
@@ -0,0 +1,20 @@
+package com.ezel.voza.global.util;
+
+import com.ezel.voza.domain.calender.Exception.CalenderNotFoundException;
+import com.ezel.voza.domain.calender.entity.Calender;
+import com.ezel.voza.domain.calender.repository.CalenderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CalenderUtil {
+
+    private final CalenderRepository calenderRepository;
+
+    public Calender findVoteById(Long id) {
+
+        return calenderRepository.findById(id)
+                .orElseThrow(() -> new CalenderNotFoundException());
+    }
+}
