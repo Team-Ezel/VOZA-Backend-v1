78733363268e520d3294e2987b7956b09d8f13fe
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-09-19 15:54:37 +0900

    #75 AdminController 매핑

--- src/main/java/com/ezel/voza/domain/user/presentation/AdminController.java
+++ src/main/java/com/ezel/voza/domain/user/presentation/AdminController.java
@@ -1,6 +1,7 @@
 package com.ezel.voza.domain.user.presentation;
 
 import com.ezel.voza.domain.user.service.GrantAdminService;
+import com.ezel.voza.domain.user.service.RevokeAdminService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,9 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdminController {
     private final GrantAdminService grantAdminService;
 
+    private final RevokeAdminService revokeAdminService;
+
     @PatchMapping("/grant")
     public ResponseEntity<Void> grantRole(@RequestParam String email) {
         grantAdminService.execute(email);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PatchMapping("/revoke")
+    public ResponseEntity<Void> revokeRole(@RequestParam String email) {
+        revokeAdminService.execute(email);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
