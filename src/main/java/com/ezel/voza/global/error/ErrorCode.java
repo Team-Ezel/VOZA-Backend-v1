f86c6f9ad214ee71994282719267cf3f39afd641
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-09-19 14:49:38 +0900

    #70 NotAdminException 작성

--- src/main/java/com/ezel/voza/global/error/ErrorCode.java
+++ src/main/java/com/ezel/voza/global/error/ErrorCode.java
@@ -48,7 +48,10 @@ public enum ErrorCode {
     AILED_UPLOAD_IMAGE("이미지 업로드에 실패했습니다", 500),
     FILE_UPLOAD_FAIL("파일 업로드에 실패했습니다.", 500),
     NOT_ALLOWED_FILE("허용되지 않은 파일 형식입니다.", 400),
-    INVALID_FORMAT_FILE("잘못된 형식의 파일입니다.", 400);
+    INVALID_FORMAT_FILE("잘못된 형식의 파일입니다.", 400),
+
+    //ADMIN
+    YOU_NOT_ADMIN("어드민 권한이 없습니다.", 403);
 
     private final String message;
     private final int status;
