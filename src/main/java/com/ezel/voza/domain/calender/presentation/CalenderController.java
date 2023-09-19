ce6c6bbb710972e921e295a10cf54818b2c483a5
hothuuk <s22014@gsm.hs.kr>
2023-09-19 15:49:35 +0900

    :memo: update 추가

--- src/main/java/com/ezel/voza/domain/calender/presentation/CalenderController.java
+++ src/main/java/com/ezel/voza/domain/calender/presentation/CalenderController.java
@@ -1,8 +1,10 @@
 package com.ezel.voza.domain.calender.presentation;
 
 import com.ezel.voza.domain.calender.presentation.dto.request.CreateCalenderRequest;
+import com.ezel.voza.domain.calender.presentation.dto.request.UpdateCalenderRequest;
 import com.ezel.voza.domain.calender.service.CreateCalenderService;
 import com.ezel.voza.domain.calender.service.DeleteCalenderService;
+import com.ezel.voza.domain.calender.service.UpdateCalenderService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,6 +20,8 @@ public class CalenderController {
 
     private final DeleteCalenderService deleteCalenderService;
 
+    private final UpdateCalenderService updateCalenderService;
+
     @PostMapping
     public ResponseEntity<Void> create(@RequestBody @Valid CreateCalenderRequest createCalenderRequest, @PathVariable Long group_id) {
         createCalenderService.execute(createCalenderRequest, group_id);
@@ -28,5 +32,11 @@ public class CalenderController {
     public ResponseEntity<Void> delete(@PathVariable Long calender_id) {
         deleteCalenderService.execute(calender_id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PatchMapping("/{calender_id}")
+    public ResponseEntity<Void> update(@RequestBody @Valid UpdateCalenderRequest updateCalenderRequest, @PathVariable Long calender_id) {
+        updateCalenderService.execute(updateCalenderRequest, calender_id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
