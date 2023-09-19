fcaaca02e08ef0c1940c8ccf5e32527791a498b4
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-09-19 14:50:45 +0900

    #70 GrantAdminService 작성

---
+++ src/main/java/com/ezel/voza/domain/user/service/GrantAdminService.java
@@ -0,0 +1,6 @@
+package com.ezel.voza.domain.user.service;
+
+public interface GrantAdminService {
+
+    void execute(String email);
+}
---
+++ src/main/java/com/ezel/voza/domain/user/service/impl/GrantAdminServiceImpl.java
@@ -0,0 +1,33 @@
+package com.ezel.voza.domain.user.service.impl;
+
+import com.ezel.voza.domain.auth.exception.UserNotFoundException;
+import com.ezel.voza.domain.user.entity.User;
+import com.ezel.voza.domain.user.entity.enums.Role;
+import com.ezel.voza.domain.user.exception.YouNotAdminException;
+import com.ezel.voza.domain.user.repository.UserRepository;
+import com.ezel.voza.domain.user.service.GrantAdminService;
+import com.ezel.voza.global.util.UserUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class GrantAdminServiceImpl implements GrantAdminService {
+
+    private final UserRepository userRepository;
+    private final UserUtil userUtil;
+
+    @Override
+    public void execute(String email) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(UserNotFoundException::new);
+
+        if (userUtil.currentUser().getRole() != Role.ROLE_ADMIN) {
+            throw new YouNotAdminException();
+        }
+
+        user.updateRole(Role.ROLE_ADMIN);
+
+        userRepository.save(user);
+    }
+}
