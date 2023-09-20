f6238a78ebcc952467986e485e3e05320bfd3815
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-09-20 19:55:05 +0900

    #76 BlackUser Entity 작성

---
+++ src/main/java/com/ezel/voza/domain/auth/entity/BlackUser.java
@@ -0,0 +1,25 @@
+package com.ezel.voza.domain.auth.entity;
+
+
+import com.ezel.voza.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class BlackUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
