e975ec4d15fdd15caa95ab655893908a27c8fba6
hothuuk <s22014@gsm.hs.kr>
2023-09-23 20:54:42 +0900

    :memo: CalenderDetailServiceImpl 작성

---
+++ src/main/java/com/ezel/voza/domain/calender/service/Impl/CalenderDetailServiceImpl.java
@@ -0,0 +1,32 @@
+package com.ezel.voza.domain.calender.service.Impl;
+
+import com.ezel.voza.domain.calender.entity.Calender;
+import com.ezel.voza.domain.calender.presentation.dto.response.CalenderDetailResponse;
+import com.ezel.voza.domain.calender.service.CalenderDetailService;
+import com.ezel.voza.global.util.CalenderUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CalenderDetailServiceImpl implements CalenderDetailService {
+
+    private final CalenderUtil calenderUtil;
+
+    @Override
+    public CalenderDetailResponse execute(Long id) {
+
+        Calender calender = calenderUtil.findCalenderById(id);
+
+        CalenderDetailResponse calenderDetailResponse = CalenderDetailResponse.builder()
+                .id(calender.getId())
+                .title(calender.getTitle())
+                .content(calender.getContent())
+                .author(calender.getUser().getNickName())
+                .startDate(calender.getStartDate())
+                .endDate(calender.getEndDate())
+                .build();
+
+        return calenderDetailResponse;
+    }
+}
