990863719eb5538fc147b2492f3f372d607796cd
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-09-23 15:10:22 +0900

    #86 DetailReportService 작성

---
+++ src/main/java/com/ezel/voza/domain/report/service/impl/DetailReportServiceImpl.java
@@ -0,0 +1,38 @@
+package com.ezel.voza.domain.report.service.impl;
+
+import com.ezel.voza.domain.report.entity.Report;
+import com.ezel.voza.domain.report.exception.NotFoundReportException;
+import com.ezel.voza.domain.report.presentation.dto.response.DetailReportResponse;
+import com.ezel.voza.domain.report.repository.ReportRepository;
+import com.ezel.voza.domain.report.service.DetailReportService;
+import com.ezel.voza.domain.user.entity.enums.Role;
+import com.ezel.voza.domain.user.exception.YouNotAdminException;
+import com.ezel.voza.global.util.UserUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class DetailReportServiceImpl implements DetailReportService {
+
+    private final ReportRepository reportRepository;
+    private final UserUtil userUtil;
+
+    @Override
+    public DetailReportResponse execute(Long reportId) {
+
+        if (userUtil.currentUser().getRole() != Role.ROLE_ADMIN) {
+            throw new YouNotAdminException();
+        }
+
+        Report report = reportRepository.findById(reportId)
+                .orElseThrow(NotFoundReportException::new);
+
+        return DetailReportResponse.builder()
+                .title(report.getTitle())
+                .content(report.getContent())
+                .name(report.getName())
+                .reportType(report.getReportType())
+                .createTime(report.getCreateTime()).build();
+    }
+}
