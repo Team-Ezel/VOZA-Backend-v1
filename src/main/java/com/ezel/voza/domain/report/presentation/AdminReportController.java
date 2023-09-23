6f627b71e397800317f3292eb84d3c4d665dc365
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-09-23 21:46:46 +0900

    #94 ️ AdminReportController 매핑

--- src/main/java/com/ezel/voza/domain/report/presentation/AdminReportController.java
+++ src/main/java/com/ezel/voza/domain/report/presentation/AdminReportController.java
@@ -1,7 +1,9 @@
 package com.ezel.voza.domain.report.presentation;
 
 import com.ezel.voza.domain.report.presentation.dto.response.DetailReportResponse;
+import com.ezel.voza.domain.report.presentation.dto.response.ListReportResponse;
 import com.ezel.voza.domain.report.service.DetailReportService;
+import com.ezel.voza.domain.report.service.ListReportService;
 import com.ezel.voza.domain.report.service.RespondReportService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,6 +17,7 @@ public class AdminReportController {
 
     private final DetailReportService detailReportService;
     private final RespondReportService respondReportService;
+    private final ListReportService listReportService;
 
     @GetMapping("/{reportId}")
     public ResponseEntity<DetailReportResponse> detailReport(@PathVariable Long reportId) {
@@ -26,5 +29,11 @@ public class AdminReportController {
     public ResponseEntity<Void> sendRespond(@PathVariable Long reportId) {
         respondReportService.execute(reportId);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<ListReportResponse> getReportList() {
+        var list = listReportService.execute();
+        return new ResponseEntity<>(list, HttpStatus.OK);
     }
 }
