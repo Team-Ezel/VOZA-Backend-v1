fcc3aac9ba280571fdfe5fa79603958526a49c8e
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-09-23 15:11:31 +0900

    #86 ReportController 매핑

--- src/main/java/com/ezel/voza/domain/report/presentation/ReportController.java
+++ src/main/java/com/ezel/voza/domain/report/presentation/ReportController.java
@@ -1,15 +1,14 @@
 package com.ezel.voza.domain.report.presentation;
 
 import com.ezel.voza.domain.report.presentation.dto.request.CreateReportRequest;
+import com.ezel.voza.domain.report.presentation.dto.response.DetailReportResponse;
+import com.ezel.voza.domain.report.service.DetailReportService;
 import com.ezel.voza.domain.report.service.SendReportService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,10 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReportController {
 
     private final SendReportService sendReportService;
+    private final DetailReportService detailReportService;
 
     @PostMapping
     public ResponseEntity<Void> createReport(@RequestBody @Valid CreateReportRequest createReportRequest) {
         sendReportService.execute(createReportRequest);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{reportId}")
+    public ResponseEntity<DetailReportResponse> detailReport(@PathVariable Long reportId) {
+        DetailReportResponse response = detailReportService.execute(reportId);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
