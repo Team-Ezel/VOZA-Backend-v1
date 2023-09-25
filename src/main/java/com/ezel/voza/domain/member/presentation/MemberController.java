6aa627caffec5df757996db0370ae4c4d21b8986
hothuuk <s22014@gsm.hs.kr>
2023-09-25 14:55:19 +0900

    :memo: kickOutMember 추가

--- src/main/java/com/ezel/voza/domain/member/presentation/MemberController.java
+++ src/main/java/com/ezel/voza/domain/member/presentation/MemberController.java
@@ -1,14 +1,12 @@
 package com.ezel.voza.domain.member.presentation;
 
 import com.ezel.voza.domain.member.presentation.response.MemberListResponse;
+import com.ezel.voza.domain.member.service.KickOutMemberService;
 import com.ezel.voza.domain.member.service.MemberListService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,9 +15,17 @@ public class MemberController {
 
     private final MemberListService memberListService;
 
+    private final KickOutMemberService kickOutMemberService;
+
     @GetMapping
     public ResponseEntity<MemberListResponse> findAllMember(@PathVariable Long group_id) {
         MemberListResponse memberListResponse = memberListService.execute(group_id);
         return new ResponseEntity<>(memberListResponse, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{user_id}")
+    public ResponseEntity<Void> kickOutMember(@PathVariable Long group_id, @PathVariable Long user_id) {
+        kickOutMemberService.execute(group_id, user_id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
