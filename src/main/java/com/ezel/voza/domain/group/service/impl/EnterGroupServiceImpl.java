450c3720fe256ffe063a46eb795f65bd35513587
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-09-25 14:00:19 +0900

    #100 EnterGroupService 작성

---
+++ src/main/java/com/ezel/voza/domain/group/service/impl/EnterGroupServiceImpl.java
@@ -0,0 +1,41 @@
+package com.ezel.voza.domain.group.service.impl;
+
+import com.ezel.voza.domain.group.entity.Group;
+import com.ezel.voza.domain.group.exception.AlreadyExistGroupException;
+import com.ezel.voza.domain.group.exception.EnterOnlyCodeException;
+import com.ezel.voza.domain.group.repository.GroupRepository;
+import com.ezel.voza.domain.group.service.EnterGroupService;
+import com.ezel.voza.domain.user.entity.User;
+import com.ezel.voza.global.util.GroupUtil;
+import com.ezel.voza.global.util.UserUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EnterGroupServiceImpl implements EnterGroupService {
+
+    private final GroupUtil groupUtil;
+    private final GroupRepository groupRepository;
+    private final UserUtil userUtil;
+
+    @Override
+    public void execute(Long groupId) {
+
+        Group group = groupUtil.findGroupById(groupId);
+
+        User user = userUtil.currentUser();
+
+        if (group.getMembers().containsKey(user)) {
+            throw new AlreadyExistGroupException();
+        }
+
+        if(group.getCanEnter()) {
+            throw new EnterOnlyCodeException();
+        }
+
+        group.putMember(user, "member");
+
+        groupRepository.save(group);
+    }
+}
