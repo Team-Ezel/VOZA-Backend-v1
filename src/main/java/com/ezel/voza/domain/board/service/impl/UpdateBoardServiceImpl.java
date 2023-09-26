e861f94667bfa9864bd9562e982e9834f5c80899
hothuuk <s22014@gsm.hs.kr>
2023-09-26 15:12:32 +0900

    :memo: 누락된 implements 및 save 추가

--- src/main/java/com/ezel/voza/domain/board/service/impl/UpdateBoardServiceImpl.java
+++ src/main/java/com/ezel/voza/domain/board/service/impl/UpdateBoardServiceImpl.java
@@ -3,6 +3,8 @@ package com.ezel.voza.domain.board.service.impl;
 import com.ezel.voza.domain.board.entity.Board;
 import com.ezel.voza.domain.board.exception.BoardAuthorMismatchException;
 import com.ezel.voza.domain.board.presentation.dto.request.UpdateBoardRequest;
+import com.ezel.voza.domain.board.repository.BoardRepository;
+import com.ezel.voza.domain.board.service.UpdateBoardService;
 import com.ezel.voza.domain.user.entity.User;
 import com.ezel.voza.global.util.BoardUtil;
 import com.ezel.voza.global.util.UserUtil;
@@ -11,12 +13,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UpdateBoardServiceImpl {
+public class UpdateBoardServiceImpl implements UpdateBoardService {
 
     private final BoardUtil boardUtil;
 
     private final UserUtil userUtil;
 
+    private final BoardRepository boardRepository;
+
+    @Override
     public void execute(Long id, UpdateBoardRequest updateBoardRequest) {
 
         Board board = boardUtil.findBoardById(id);
@@ -28,5 +33,7 @@ public class UpdateBoardServiceImpl {
         }
 
         board.update(updateBoardRequest);
+
+        boardRepository.save(board);
     }
 }
