cf62fd9347089e85d9154145d532d46572102e83
hothuuk <s22014@gsm.hs.kr>
2023-09-26 15:12:11 +0900

    :memo: 누락된 implements 추가

--- src/main/java/com/ezel/voza/domain/board/service/impl/GetBoardDetailServiceImpl.java
+++ src/main/java/com/ezel/voza/domain/board/service/impl/GetBoardDetailServiceImpl.java
@@ -2,16 +2,18 @@ package com.ezel.voza.domain.board.service.impl;
 
 import com.ezel.voza.domain.board.entity.Board;
 import com.ezel.voza.domain.board.presentation.dto.response.DetailBoardResponse;
+import com.ezel.voza.domain.board.service.GetBoardDetailService;
 import com.ezel.voza.global.util.BoardUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class GetBoardDetailServiceImpl {
+public class GetBoardDetailServiceImpl implements GetBoardDetailService {
 
     private final BoardUtil boardUtil;
 
+    @Override
     public DetailBoardResponse execute(Long id) {
 
         Board board = boardUtil.findBoardById(id);
