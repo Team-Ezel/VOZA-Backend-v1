06070779fd5234a539ac5eb1286dd40503d0754c
hothuuk <s22014@gsm.hs.kr>
2023-09-26 15:09:32 +0900

    :memo: findALlByGroup 추가

--- src/main/java/com/ezel/voza/domain/board/repository/BoardRepository.java
+++ src/main/java/com/ezel/voza/domain/board/repository/BoardRepository.java
@@ -1,8 +1,12 @@
 package com.ezel.voza.domain.board.repository;
 
 import com.ezel.voza.domain.board.entity.Board;
+import com.ezel.voza.domain.group.entity.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
+    List<Board> findALlByGroup(Group group);
 }
