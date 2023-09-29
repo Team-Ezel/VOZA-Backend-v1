0170482873e59ed48bad5628f58eabd5eb9be716
hothuuk <s22014@gsm.hs.kr>
2023-09-29 18:09:23 +0900

    :memo: BaseTimeEntity 상속

--- src/main/java/com/ezel/voza/domain/board/entity/Board.java
+++ src/main/java/com/ezel/voza/domain/board/entity/Board.java
@@ -3,19 +3,16 @@ package com.ezel.voza.domain.board.entity;
 import com.ezel.voza.domain.board.entity.enums.BoardType;
 import com.ezel.voza.domain.board.presentation.dto.request.UpdateBoardRequest;
 import com.ezel.voza.domain.group.entity.Group;
+import com.ezel.voza.global.entity.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Board {
+public class Board extends BaseTimeEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -34,22 +31,12 @@ public class Board {
     @Enumerated(EnumType.STRING)
     private BoardType boardType;
 
-    @CreatedDate
-    private LocalDateTime createdDate;
-
-    @LastModifiedDate
-    private LocalDateTime editedDate;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "group_id")
     private Group group;
 
-    @Column(name = "board_file_url")
-    private String url;
-
     public void update(UpdateBoardRequest updateBoardRequest) {
         this.title = updateBoardRequest.getTitle();
         this.content = updateBoardRequest.getContent();
-        this.editedDate = LocalDateTime.now();
     }
 }
