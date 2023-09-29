1bf5f538b768368c22a15c2b30675af1fcb1b8c4
hothuuk <s22014@gsm.hs.kr>
2023-09-29 18:11:45 +0900

    :memo: file 삭제

--- src/main/java/com/ezel/voza/domain/board/service/CreateBoardService.java
+++ src/main/java/com/ezel/voza/domain/board/service/CreateBoardService.java
@@ -1,9 +1,8 @@
 package com.ezel.voza.domain.board.service;
 
 import com.ezel.voza.domain.board.presentation.dto.request.CreateBoardRequest;
-import org.springframework.web.multipart.MultipartFile;
 
 public interface CreateBoardService {
 
-    void execute(CreateBoardRequest createBoardRequest, Long groupId, MultipartFile file);
+    void execute(CreateBoardRequest createBoardRequest, Long groupId);
 }
