29cf54d024aca3bc73df896b661d84a81c23372a
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-09-30 22:59:04 +0900

    #116 RoomChat과 Room 분리

--- src/main/generated/com/ezel/voza/domain/chat/entity/QRoom.java
+++ src/main/generated/com/ezel/voza/domain/chat/entity/QRoom.java
@@ -24,11 +24,11 @@ public class QRoom extends EntityPathBase<Room> {
 
     public final com.ezel.voza.domain.group.entity.QGroup group;
 
-    public final QRoom_LastChat lastChat;
+    public final StringPath lastChat = createString("lastChat");
+
+    public final DateTimePath<java.time.LocalDateTime> lastSendAt = createDateTime("lastSendAt", java.time.LocalDateTime.class);
 
     public final NumberPath<Long> roomId = createNumber("roomId", Long.class);
-
-    public final MapPath<String, String, StringPath> stringStringMap = this.<String, String, StringPath>createMap("stringStringMap", String.class, String.class, StringPath.class);
 
     public QRoom(String variable) {
         this(Room.class, forVariable(variable), INITS);
@@ -49,7 +49,6 @@ public class QRoom extends EntityPathBase<Room> {
     public QRoom(Class<? extends Room> type, PathMetadata metadata, PathInits inits) {
         super(type, metadata, inits);
         this.group = inits.isInitialized("group") ? new com.ezel.voza.domain.group.entity.QGroup(forProperty("group")) : null;
-        this.lastChat = inits.isInitialized("lastChat") ? new QRoom_LastChat(forProperty("lastChat")) : null;
     }
 
 }
