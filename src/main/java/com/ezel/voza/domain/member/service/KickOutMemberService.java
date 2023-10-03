26cfd7b01256e6631dfb4cf5bacdade523540223
hothuuk <s22014@gsm.hs.kr>
2023-10-03 17:03:19 +0900

    :memo: BlackMember에 추방당한 유저를 추가

--- src/main/java/com/ezel/voza/domain/member/service/KickOutMemberService.java
+++ src/main/java/com/ezel/voza/domain/member/service/KickOutMemberService.java
@@ -2,5 +2,5 @@ package com.ezel.voza.domain.member.service;
 
 public interface KickOutMemberService {
 
-    void execute(Long groupId, Long userId);
+    void execute(Long groupId, Long userId, String kickOutTime);
 }
