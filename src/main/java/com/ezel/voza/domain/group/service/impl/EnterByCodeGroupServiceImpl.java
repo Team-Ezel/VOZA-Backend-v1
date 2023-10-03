cb147e1130948e3c9ddc72e50b0b827dff58207a
hothuuk <s22014@gsm.hs.kr>
2023-10-03 17:01:35 +0900

    :memo: 추방당한 유저 검사 기능 추가

--- src/main/java/com/ezel/voza/domain/group/service/impl/EnterByCodeGroupServiceImpl.java
+++ src/main/java/com/ezel/voza/domain/group/service/impl/EnterByCodeGroupServiceImpl.java
@@ -2,19 +2,19 @@ package com.ezel.voza.domain.group.service.impl;
 
 import com.ezel.voza.domain.group.entity.Group;
 import com.ezel.voza.domain.group.entity.GroupInvite;
-import com.ezel.voza.domain.group.exception.AlreadyExistGroupException;
-import com.ezel.voza.domain.group.exception.GroupNotFoundException;
-import com.ezel.voza.domain.group.exception.MisMatchInviteCodeException;
-import com.ezel.voza.domain.group.exception.NotAllowedEnterBanGroupException;
+import com.ezel.voza.domain.group.exception.*;
 import com.ezel.voza.domain.group.presentation.dto.request.EnterGroupRequest;
 import com.ezel.voza.domain.group.repository.GroupInviteRepository;
 import com.ezel.voza.domain.group.repository.GroupRepository;
 import com.ezel.voza.domain.group.service.EnterByCodeGroupService;
+import com.ezel.voza.domain.member.entity.BlackMember;
+import com.ezel.voza.domain.member.repository.BlackMemberRepository;
 import com.ezel.voza.domain.user.entity.User;
 import com.ezel.voza.global.util.UserUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Service
@@ -24,12 +24,12 @@ public class EnterByCodeGroupServiceImpl implements EnterByCodeGroupService {
     private final GroupInviteRepository groupInviteRepository;
     private final GroupRepository groupRepository;
     private final UserUtil util;
+    private final BlackMemberRepository blackMemberRepository;
 
     @Override
     public void execute(EnterGroupRequest enterGroupRequest) {
 
         User user = util.currentUser();
-
 
         GroupInvite invite = groupInviteRepository.findById(enterGroupRequest.getEmail())
                 .orElseThrow(GroupNotFoundException::new);
@@ -48,11 +48,25 @@ public class EnterByCodeGroupServiceImpl implements EnterByCodeGroupService {
                 throw new AlreadyExistGroupException();
             }
 
+            checkBlackMember(group, user);
+
             group.putMember(user, "member");
 
             groupRepository.save(group);
         } else {
             throw new MisMatchInviteCodeException();
+        }
+    }
+
+    public void checkBlackMember(Group group, User user) {
+        BlackMember blackMember = blackMemberRepository.findByGroupAndUser(group, user);
+
+        if (blackMember != null) {
+            LocalDateTime currentTime = LocalDateTime.now();
+
+            if (currentTime.isBefore(blackMember.getKickOutTime())) {
+                throw new KickOutUserException();
+            }
         }
     }
 }
--- src/main/java/com/ezel/voza/domain/group/service/impl/EnterGroupServiceImpl.java
+++ src/main/java/com/ezel/voza/domain/group/service/impl/EnterGroupServiceImpl.java
@@ -3,14 +3,19 @@ package com.ezel.voza.domain.group.service.impl;
 import com.ezel.voza.domain.group.entity.Group;
 import com.ezel.voza.domain.group.exception.AlreadyExistGroupException;
 import com.ezel.voza.domain.group.exception.EnterOnlyCodeException;
+import com.ezel.voza.domain.group.exception.KickOutUserException;
 import com.ezel.voza.domain.group.exception.NotAllowedEnterBanGroupException;
 import com.ezel.voza.domain.group.repository.GroupRepository;
 import com.ezel.voza.domain.group.service.EnterGroupService;
+import com.ezel.voza.domain.member.entity.BlackMember;
+import com.ezel.voza.domain.member.repository.BlackMemberRepository;
 import com.ezel.voza.domain.user.entity.User;
 import com.ezel.voza.global.util.GroupUtil;
 import com.ezel.voza.global.util.UserUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +24,7 @@ public class EnterGroupServiceImpl implements EnterGroupService {
     private final GroupUtil groupUtil;
     private final GroupRepository groupRepository;
     private final UserUtil userUtil;
+    private final BlackMemberRepository blackMemberRepository;
 
     @Override
     public void execute(Long groupId) {
@@ -39,8 +45,22 @@ public class EnterGroupServiceImpl implements EnterGroupService {
             throw new NotAllowedEnterBanGroupException();
         }
 
+        checkBlackMember(group, user);
+
         group.putMember(user, "member");
 
         groupRepository.save(group);
+    }
+
+    public void checkBlackMember(Group group, User user) {
+        BlackMember blackMember = blackMemberRepository.findByGroupAndUser(group, user);
+
+        if (blackMember != null) {
+            LocalDateTime currentTime = LocalDateTime.now();
+
+            if (currentTime.isBefore(blackMember.getKickOutTime())) {
+                throw new KickOutUserException();
+            }
+        }
     }
 }
