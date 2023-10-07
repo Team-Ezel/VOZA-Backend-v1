ecbb16edaad4f4b7473978484c4bf9d6932c6a00
hothuuk <s22014@gsm.hs.kr>
2023-10-07 23:54:43 +0900

    :memo: optionId로 변경

--- src/main/java/com/ezel/voza/domain/vote/service/AddCountService.java
+++ src/main/java/com/ezel/voza/domain/vote/service/AddCountService.java
@@ -1,8 +1,6 @@
 package com.ezel.voza.domain.vote.service;
 
-import com.ezel.voza.domain.vote.presentation.dto.request.AddCountRequest;
-
 public interface AddCountService {
 
-    void execute(Long vote_id, AddCountRequest addCountRequest);
+    void execute(Long voteId, Long optionId);
 }
