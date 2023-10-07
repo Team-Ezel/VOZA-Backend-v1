e3a8a4a6598bf36cb276310d7aac4eeb5f3b9400
hothuuk <s22014@gsm.hs.kr>
2023-10-07 17:06:28 +0900

    :memo: 필드 추가

--- src/main/java/com/ezel/voza/domain/vote/presentation/dto/response/VoteResponse.java
+++ src/main/java/com/ezel/voza/domain/vote/presentation/dto/response/VoteResponse.java
@@ -1,9 +1,12 @@
 package com.ezel.voza.domain.vote.presentation.dto.response;
 
 import com.ezel.voza.domain.vote.entity.Vote;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Builder
@@ -14,11 +17,18 @@ public class VoteResponse {
 
     private String title;
 
+    private String author;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH-mm-ss")
+    private LocalDateTime createdDate;
+
     public static VoteResponse toResponse(Vote vote) {
 
         return VoteResponse.builder()
                 .id(vote.getId())
                 .title(vote.getTitle())
+                .author(vote.getUser().getNickName())
+                .createdDate(vote.getCreatedDate())
                 .build();
     }
 }
