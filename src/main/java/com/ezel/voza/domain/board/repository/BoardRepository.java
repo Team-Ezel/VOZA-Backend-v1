e3ecc94800fe3d8a87510ee2d3b984bf986269fc
hothuuk <s22014@gsm.hs.kr>
2023-10-10 12:08:41 +0900

    :memo: 마지막에 생성된 게시판을 찾는 jpa 추가

--- src/main/java/com/ezel/voza/domain/board/repository/BoardRepository.java
+++ src/main/java/com/ezel/voza/domain/board/repository/BoardRepository.java
@@ -5,8 +5,11 @@ import com.ezel.voza.domain.group.entity.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
     List<Board> findALlByGroup(Group group);
+
+    Optional<Board> findFirstByOrderByIdDesc();
 }
