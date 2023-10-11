a77942972679276c248006f71244be5e1c54a18b
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-10-11 16:14:59 +0900

    #137 WebConfig에 LogginInterCeptor 추가

---
+++ src/main/java/com/ezel/voza/global/config/WebConfiguration.java
@@ -0,0 +1,19 @@
+package com.ezel.voza.global.config;
+
+import com.ezel.voza.global.interceptor.LoggingInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    private final LoggingInterceptor loggingInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor);
+    }
+}
