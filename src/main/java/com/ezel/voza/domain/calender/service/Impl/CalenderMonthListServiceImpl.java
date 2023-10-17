6eb841edc9a035a27615cf0e3dfa2f8ccab2cbeb
hothuuk <s22014@gsm.hs.kr>
2023-10-17 21:00:31 +0900

    :memo: 바뀐 controller에 맞게 변수 변경 및 변수 인라인화

--- src/main/java/com/ezel/voza/domain/calender/service/Impl/CalenderMonthListServiceImpl.java
+++ src/main/java/com/ezel/voza/domain/calender/service/Impl/CalenderMonthListServiceImpl.java
@@ -1,7 +1,6 @@
 package com.ezel.voza.domain.calender.service.Impl;
 
 import com.ezel.voza.domain.calender.entity.Calender;
-import com.ezel.voza.domain.calender.presentation.dto.request.GetDateRequest;
 import com.ezel.voza.domain.calender.presentation.dto.response.CalenderMonthListResponse;
 import com.ezel.voza.domain.calender.presentation.dto.response.CalenderMonthResponse;
 import com.ezel.voza.domain.calender.repository.CalenderRepository;
@@ -23,20 +22,18 @@ public class CalenderMonthListServiceImpl implements CalenderMonthListService {
     private final GroupUtil groupUtil;
 
     @Override
-    public CalenderMonthListResponse execute(Long id, GetDateRequest getDateRequest) {
+    public CalenderMonthListResponse execute(Long id, String searchDate) {
 
         Group group = groupUtil.findGroupById(id);
 
-        List<Calender> calenderList = calenderRepository.findAllByGroupAndDate(group, getDateRequest.getDate());
+        List<Calender> calenderList = calenderRepository.findAllByGroupAndDate(group, searchDate);
 
-        CalenderMonthListResponse calenderMonthListResponse = CalenderMonthListResponse.builder()
+        return CalenderMonthListResponse.builder()
                 .calenderMonthResponses(
                         calenderList.stream()
                                 .map(CalenderMonthResponse::toResponse)
                                 .collect(Collectors.toList())
                 )
                 .build();
-
-        return calenderMonthListResponse;
     }
 }
