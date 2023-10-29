61f14a09e8dbe0f0a6b6f904beeaa81d2fa93381
hothuuk <s22014@gsm.hs.kr>
2023-10-29 13:27:05 +0900

    :memo: getOtherProfile 추가

--- src/main/java/com/ezel/voza/domain/user/presentation/UserController.java
+++ src/main/java/com/ezel/voza/domain/user/presentation/UserController.java
@@ -1,11 +1,13 @@
 package com.ezel.voza.domain.user.presentation;
 
 import com.ezel.voza.domain.user.presentation.dto.response.ProfileResponse;
+import com.ezel.voza.domain.user.service.GetOtherProfileService;
 import com.ezel.voza.domain.user.service.GetProfileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +18,17 @@ public class UserController {
 
     private final GetProfileService getProfileService;
 
+    private final GetOtherProfileService getOtherProfileService;
+
     @GetMapping("/profile")
     public ResponseEntity<ProfileResponse> getProfile() {
         ProfileResponse profileResponse = getProfileService.execute();
+        return new ResponseEntity<>(profileResponse, HttpStatus.OK);
+    }
+
+    @GetMapping("/otherProfile/{userId}")
+    public ResponseEntity<ProfileResponse> getOtherProfile(@PathVariable Long userId) {
+        ProfileResponse profileResponse = getOtherProfileService.execute(userId);
         return new ResponseEntity<>(profileResponse, HttpStatus.OK);
     }
 }
