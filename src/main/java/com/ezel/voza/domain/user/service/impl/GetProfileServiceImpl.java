29fae8d159b387862e0843347db6036583c9822e
hothuuk <s22014@gsm.hs.kr>
2023-10-29 13:27:58 +0900

    :memo: 바뀐 response에 맞게 수정

--- src/main/java/com/ezel/voza/domain/user/service/impl/GetProfileServiceImpl.java
+++ src/main/java/com/ezel/voza/domain/user/service/impl/GetProfileServiceImpl.java
@@ -1,11 +1,17 @@
 package com.ezel.voza.domain.user.service.impl;
 
+import com.ezel.voza.domain.group.entity.Group;
+import com.ezel.voza.domain.group.repository.GroupRepository;
 import com.ezel.voza.domain.user.entity.User;
+import com.ezel.voza.domain.user.presentation.dto.response.JoinedGroupResponse;
 import com.ezel.voza.domain.user.presentation.dto.response.ProfileResponse;
 import com.ezel.voza.domain.user.service.GetProfileService;
 import com.ezel.voza.global.annotation.ReadOnlyService;
 import com.ezel.voza.global.util.UserUtil;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ReadOnlyService
 @RequiredArgsConstructor
@@ -13,14 +19,23 @@ public class GetProfileServiceImpl implements GetProfileService {
 
     private final UserUtil userUtil;
 
+    private final GroupRepository groupRepository;
+
     @Override
     public ProfileResponse execute() {
         User user = userUtil.currentUser();
+
+        List<Group> groupList = groupRepository.findGroupsByMembers(user);
 
         return ProfileResponse.builder()
                 .email(user.getEmail())
                 .profileUrl(user.getProfileUrl())
                 .nickName(user.getNickName())
+                .joinedGroupList(
+                        groupList.stream()
+                                .map(JoinedGroupResponse::toResponse)
+                                .collect(Collectors.toList())
+                )
                 .build();
     }
 }
