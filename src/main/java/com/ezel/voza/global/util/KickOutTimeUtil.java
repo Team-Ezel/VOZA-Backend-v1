9412af03c574fd772415f621370cf0de13e467fc
hothuuk <s22014@gsm.hs.kr>
2023-10-30 09:43:45 +0900

    :memo: 추방 기간 추가

--- src/main/java/com/ezel/voza/global/util/KickOutTimeUtil.java
+++ src/main/java/com/ezel/voza/global/util/KickOutTimeUtil.java
@@ -11,6 +11,11 @@ public class KickOutTimeUtil {
 
     public static long getSeconds(KickOutTime kickOutTime) {
         return switch (kickOutTime) {
+            case FIVE_HOUR_STOP -> 5 * 60 * 60; // 5시간
+            case EIGHT_HOUR_STOP -> 8 * 60 * 60; // 8시간
+            case TWELVE_HOUR_STOP -> 12 * 60 * 60; // 12시간
+            case ONE_DAY_STOP -> 24 * 60 * 60; // 1일
+            case THREE_DAY_STOP -> 3 * 24 * 60 * 60; // 3일
             case SEVEN_DAY_STOP -> 7 * 24 * 60 * 60; // 7일
             case THIRTY_DAY_STOP -> 30 * 24 * 60 * 60; // 30일
             case ALL_DAY_STOP -> Long.MAX_VALUE; // 무기한 추방
