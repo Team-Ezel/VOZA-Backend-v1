da64c0a5c67710727a58491fffeb7f928c478160
hothuuk <s22014@gsm.hs.kr>
2023-10-30 12:29:21 +0900

    :memo: 공통 부분 묶기

--- src/main/java/com/ezel/voza/global/util/KickOutTimeUtil.java
+++ src/main/java/com/ezel/voza/global/util/KickOutTimeUtil.java
@@ -10,15 +10,19 @@ import org.springframework.stereotype.Component;
 public class KickOutTimeUtil {
 
     public static long getSeconds(KickOutTime kickOutTime) {
+
+        final int SECONDS_IN_AN_HOUR = 60 * 60;
+        final int SECONDS_IN_A_DAY = 24 * SECONDS_IN_AN_HOUR;
+
         return switch (kickOutTime) {
-            case FIVE_HOUR_STOP -> 5 * 60 * 60; // 5시간
-            case EIGHT_HOUR_STOP -> 8 * 60 * 60; // 8시간
-            case TWELVE_HOUR_STOP -> 12 * 60 * 60; // 12시간
-            case ONE_DAY_STOP -> 24 * 60 * 60; // 1일
-            case THREE_DAY_STOP -> 3 * 24 * 60 * 60; // 3일
-            case SEVEN_DAY_STOP -> 7 * 24 * 60 * 60; // 7일
-            case THIRTY_DAY_STOP -> 30 * 24 * 60 * 60; // 30일
-            case PERMANENT_STOP -> Long.MAX_VALUE; // 무기한 추방
+            case FIVE_HOUR_STOP -> 5 * SECONDS_IN_AN_HOUR;
+            case EIGHT_HOUR_STOP -> 8 * SECONDS_IN_AN_HOUR;
+            case TWELVE_HOUR_STOP -> 12 * SECONDS_IN_AN_HOUR;
+            case ONE_DAY_STOP -> SECONDS_IN_A_DAY;
+            case THREE_DAY_STOP -> 3 * SECONDS_IN_A_DAY;
+            case SEVEN_DAY_STOP -> 7 * SECONDS_IN_A_DAY;
+            case THIRTY_DAY_STOP -> 30 * SECONDS_IN_A_DAY;
+            case PERMANENT_STOP -> Long.MAX_VALUE;
             default -> throw new InvaildFormatException();
         };
     }
